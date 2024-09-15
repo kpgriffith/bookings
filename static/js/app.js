f4251449cfd34c106a34f9dbc7f446bc1dbdaca7
Kevin Griffith <kpgriffith@me.com>
2024-09-15 07:28:26 -0500

    finish up db

--- static/js/app.js
+++ static/js/app.js
@@ -110,3 +110,75 @@ function Prompt(){
       custom: custom,
     }
 }
+
+function abc(c){
+  const {
+    room_id = "",
+  } = c;
+  let html = `
+  <form id="check-availability-form" action="" method="post" novalidate class="needs-validation">
+      <div class="row">
+      <div class="col">
+          <div class="row" id="reservation-dates-modal">
+          <div class="col">
+              <input disabled required class="form-control" type="text" name="start" id="start" placeholder="Arrival">
+          </div>
+          <div class="col">
+              <input disabled required class="form-control" type="text" name="end" id="end" placeholder="Departure">
+          </div>
+          </div>
+      </div>
+      </div>
+  </form>
+  `
+  Prompt().custom({
+      msg: html,
+      title: "Choose your Dates",
+      willOpen: () => {
+        const elem = document.getElementById('reservation-dates-modal');
+        const rp = new DateRangePicker(elem, {
+          // ...options
+          format: "yyyy-mm-dd",
+          showOnFocus: true,
+          minDate: new Date(),
+        })
+      },
+      didOpen: () => {
+        document.getElementById('start').removeAttribute('disabled');
+        document.getElementById('end').removeAttribute('disabled');
+      },
+      callback: function(result){
+          let form = document.getElementById("check-availability-form");
+          let formData = new FormData(form);
+          formData.append("csrf_token", "{{.CSRFToken}}");
+          formData.append("room_id", room_id);
+
+          fetch('/search-availability-json', {
+              method: "post",
+              body: formData,
+          })
+              .then(response => response.json())
+              .then(data => {
+                  if (data.ok) {
+                      Prompt().custom({
+                          icon: "success",
+                          msg: '<p>Room is available!</p>'
+                              + '<p><a href="/book-room?id='
+                              + data.room_id
+                              + '&start='
+                              + data.start_date
+                              + '&end='
+                              + data.end_date
+                              + '" class="btn btn-primary">'
+                              + 'Book now!</a></p>',
+                          showConfirmButton: false,
+                      })
+                  } else {
+                      Prompt().error({
+                          msg: "No availability",
+                      })
+                  }
+              })
+      }
+  });
+}
